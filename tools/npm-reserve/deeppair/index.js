adf0b3a7a89a9a90d868c96b6d9330755eccbfe5
mitchjablonski <22137408+mitchjablonski@users.noreply.github.com>
2026-05-19 21:59:41 -0400

    Drop deep-pairing + deeppair from reserve set (community-norm scope)

--- tools/npm-reserve/deeppair/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable */
-process.stderr.write(
-  "\n  This is a typosquat-defense placeholder. The canonical name is `deeppairing` (full name, no truncation).\n" +
-  "  Real project: https://github.com/deeppairing/deeppairing\n\n",
-);
-process.exit(1);
