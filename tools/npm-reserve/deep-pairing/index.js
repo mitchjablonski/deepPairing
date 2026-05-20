3fce704c5a2efb4668e87b8baa9a4f673fd595b1
Mitch <mitchjablonski@gmail.com>
2026-05-19 21:59:41 -0400

    Drop deep-pairing + deeppair from reserve set (community-norm scope)

--- tools/npm-reserve/deep-pairing/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable */
-process.stderr.write(
-  "\n  This is a typosquat-defense placeholder. The canonical name is `deeppairing` (no hyphen).\n" +
-  "  Real project: https://github.com/deeppairing/deeppairing\n\n",
-);
-process.exit(1);
